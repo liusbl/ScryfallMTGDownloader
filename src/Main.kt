77b275bbbf024fbc4758934ad5d0489cb5b886c8
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2022-12-29 15:32:19 +0200

    Reading page content

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,54 @@
-fun main() {
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
+
+fun main() {
+    val file = File("out.txt")
+    println("Hello")
+
+    val json = getPageJson("https://api.scryfall.com/cards/search?q=t%3Abasic+unique%3Aprints+-is%3Afullart&unique=cards&as=grid&order=name")
+
+    val cards = json["data"].asJsonArray
+
+    cards.forEach { card ->
+        card as JsonObject
+        val name = card["name"].toString().filter { it != '\"' }
+        val set = card["set"].toString().filter { it != '\"' }
+        val collectorNumber = card["collector_number"].toString().filter { it != '\"' }
+        if (collectorNumber.find { !it.isDigit() && !it.isLetter() } != null) {
+            throw Exception("Invalid card collector number: $collectorNumber")
+        }
+        val imageDownloadUrl = (card["image_uris"] as JsonObject)["png"]
+
+        println("Filename: $name-$collectorNumber-$set, url: $imageDownloadUrl")
+    }
+
+    if (json.has("next_page")) {
+        // Finished
+    } else {
+        // Finished
+    }
+}
+
+private fun getPageJson(url: String): JsonObject {
+    val con: HttpURLConnection = URL(url).openConnection() as HttpURLConnection
+    con.requestMethod = "GET"
+
+    con.doOutput = true
+
+    val response = StringBuilder()
+    BufferedReader(InputStreamReader(con.inputStream)).use { reader ->
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            response.append(line)
+        }
+    }
+
+    val gson = Gson()
+    return gson.fromJson(response.toString(), JsonObject::class.java)
 }
