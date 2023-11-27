466721d2ba2ce9c2ee9368e40a4794026be3cb16
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2023-11-27 10:52:46 +0200

    Download cards with multiple faces

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 import com.google.gson.Gson
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -46,25 +47,15 @@ private fun downloadCards(url: String, page: Int, result: DownloadResult): Downl
         val set = card["set"].toString().filter { it != '\"' }
         val collectorNumber = card["collector_number"].toString().filter { it != '\"' }
         if (collectorNumber.find { !it.isDigit() && !it.isLetter() && it != '★' } != null) {
-            throw Exception("Invalid card collector number: $collectorNumber")
+            error("Invalid card collector number: $collectorNumber")
         }
         val imageName = "$name-$collectorNumber-$set"
 
         val imageUris = card["image_uris"] as? JsonObject
         if (imageUris == null) {
-            System.err.println("Card does not contain \"image_uris\" object. imageName: $imageName")
-            return@forEach
-        }
-        val imageDownloadUrl = imageUris["png"].toString().trim('\"')
-
-        if (Files.exists(Paths.get("out\\Lands\\$imageName.png"))) {
-            System.err.println("Image already exists: $imageName")
+            downloadMultipleFaceCards(card, imageName, collectorNumber, set, downloadedCardList)
         } else {
-            URL(imageDownloadUrl).openStream().use { inputStream ->
-                Files.copy(inputStream, Paths.get("out\\Lands\\$imageName.png"))
-                downloadedCardList.add("$imageName.png")
-                println("Stored image. Filename: $imageName, url: $imageDownloadUrl")
-            }
+            downloadImage(imageUris, imageName, downloadedCardList)
         }
     }
 
@@ -97,6 +88,54 @@ private fun getPageJson(url: String): JsonObject {
 
     val gson = Gson()
     return gson.fromJson(response.toString(), JsonObject::class.java)
+}
+
+private fun downloadMultipleFaceCards(
+    card: JsonObject,
+    imageName: String,
+    collectorNumber: String,
+    set: String,
+    downloadedCardList: MutableList<String>
+) {
+    // Some cards have 2 faces, back and front, thus `image_uris` exists only in those inner elements. Example: "Plains // Plains-21-rex".
+    val cardFaces = card["card_faces"] as? JsonArray
+    if (cardFaces == null) {
+        error("Card does not contain \"image_uris\" object AND does not contain \"card_faces\". Investigate! imageName: $imageName")
+    } else {
+        if (cardFaces.size() != 2) {
+            error("Card does not contain \"image_uris\" object but contains ${cardFaces.size()} \"card_faces\". Very unusual. Investigate! imageName: $imageName")
+        }
+        cardFaces.forEachIndexed { index, cardFace ->
+            cardFace as JsonObject
+            val cardFaceImageUris = cardFace["image_uris"] as? JsonObject
+            if (cardFaceImageUris == null) {
+                error("Card does not contain \"image_uris\" object and contains \"card_faces\" BUT does not contains inner cardFace \"image_uris\". Investigate! imageName: $imageName")
+            } else {
+                // The "upper" card name is not great since it looks like "Plains // Plains",
+                //  so we use the face name.
+                val cardFaceName = cardFace["name"].toString().filter { it != '\"' }
+                val cardFaceImageName = "$cardFaceName-$collectorNumber-$set-face-${index + 1}"
+                downloadImage(cardFaceImageUris, cardFaceImageName, downloadedCardList)
+            }
+        }
+    }
+}
+
+private fun downloadImage(
+    imageUris: JsonObject,
+    imageName: String,
+    downloadedCardList: MutableList<String>
+) {
+    val imageDownloadUrl = imageUris["png"].toString().trim('\"')
+    if (Files.exists(Paths.get("out\\Lands\\$imageName.png"))) {
+        System.err.println("Image already exists: $imageName")
+    } else {
+        URL(imageDownloadUrl).openStream().use { inputStream ->
+            Files.copy(inputStream, Paths.get("out\\Lands\\$imageName.png"))
+            downloadedCardList.add("$imageName.png")
+            println("Stored image. Filename: $imageName, url: $imageDownloadUrl")
+        }
+    }
 }
 
 data class DownloadResult(
